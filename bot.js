68e34720d3ced17f3cfc4830167988427362f0d8
Taylor Buckner <taylora.buckner@gmail.com>
2017-10-28 14:52:32 -0500

    added a periodical follow back feature

--- bot.js
+++ bot.js
@@ -1,15 +1,21 @@
 console.log('The bot is starting...');
 var hourMultiplier = (1000 * 60 * 60);
-var myInterval = (hourMultiplier*3);
-console.log('the tweets will go out once every ' + (myInterval / (1000 * 60)) + ' minute(s), or every ' + (myInterval / (1000 * 60 * 60)) + ' hour(s)');
+var randomTweetInterval = (hourMultiplier * 3);
+var periodicalFollowInterval = (hourMultiplier * 6);
+console.log('the tweets will go out once every ' + (randomTweetInterval / (1000 * 60)) + ' minute(s), or every ' + (randomTweetInterval / (1000 * 60 * 60)) + ' hour(s)');
+console.log('the bot will double check for missed followers every ' + (periodicalFollowInterval / hourMultiplier) + " hour(s)");
 
-// setInterval(bibleTrendTweet, myInterval);
+/* 
+RANDOM TWEET 
+ */
 
 var fs = require('fs');
 var download = require('download');
 
 downloadImage();
-setInterval(downloadImage, myInterval);
+periodicalFollowHandler();
+setInterval(downloadImage, randomTweetInterval);
+setInterval(periodicalFollowHandler, periodicalFollowInterval);
 
 function downloadImage() {
   download('https://picsum.photos/1920/1080?random', 'img').then(() => {
@@ -50,6 +56,70 @@ var sParams = {
   geocode: '30.2672 97.7431 100mi',
   count: 10
 }
+
+/* 
+DAILY FOLLOW BACK
+ */
+var toFollowBack = [];
+
+
+function periodicalFollowHandler() {
+  getNeedFollows(1, function (data) {
+    followBack(data);
+  });
+}
+
+function getNeedFollows(e, callback) {
+  var unused = e;
+  T.get('followers/list', { count: 200 }, function (err, data, response) {
+    var userArray = data.users;
+    var userCount = userArray.length;
+
+    if (err) {
+      console.log(err);
+    } else {
+      for (i = 0; i < userCount; i++) {
+        var id = userArray[i].id;
+        var screenName = userArray[i].screen_name;
+        var isFollowing = userArray[i].following;
+
+        if (isFollowing !== true) {
+          var thisGuy = { id: id, screenName: screenName, isFollowing: isFollowing }
+          toFollowBack.push(thisGuy);
+        }
+      }
+    }
+    callback(toFollowBack);
+  })
+}
+
+//Follow Back
+function followBack(object) {
+  console.log('followBack scope...');
+
+  for (i = 0; i < object.length; i++) {
+    var thisID = object[i].id;
+    var thisUser = object[i].screenName;
+    T.post('friendships/create', { id: thisID }, function (err, data, response) {
+      if (!err) {
+        console.log('You followed User ID: ' + thisID + '; Scree Name: ' + thisUser);
+      } else if (err === 108) {
+        console.log('Trying agian using Screen Name');
+        T.post('friendships/create', { screen_name: screenName }, function (err, data, response) {
+          if (err) {
+            console.log('couldnt fuckin do it bro...');
+            console.log(err);
+          }
+        })
+      }
+    });
+  }
+}
+
+
+/* 
+FOLLOW BACK STREAM 
+*/
 
 // search for shit...not in use
 //T.get('search/tweets', sParams, gotSearch);
